fe58566511a5e2b853c4d4de77f0dda9b09025e3
Guzik98 <guzkac2@gmail.com>
2020-11-19 22:33:24 +0100

    reset password work

--- lab2_aplikacje_internetowe/lab2_aplikacje_internetowe/settings.py
+++ lab2_aplikacje_internetowe/lab2_aplikacje_internetowe/settings.py
@@ -126,3 +126,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOFIN_URL = 'login'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'guzikkacper58@gmail.com'
+EMAIL_HOST_PASSWORD = 'wiedzmin2'
